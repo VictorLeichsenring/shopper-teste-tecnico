f8777cd382bbb2adf0f431f348e09bbd978c858a
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-11-22 01:20:22 -0300

    Criado migration para drivers

---
+++ backend/src/migrations/20241122040406-create-driver.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Drivers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      vehicle: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      rating: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      comment: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Drivers')
+  }
+};
