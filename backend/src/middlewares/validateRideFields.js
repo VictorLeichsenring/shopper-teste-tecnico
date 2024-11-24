ee246eb6300ff8c26b35013c987521f0d2b891d5
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-11-24 10:45:45 -0300

    adaptando mensagem de erro

--- backend/src/middlewares/validateRideFields.js
+++ backend/src/middlewares/validateRideFields.js
@@ -7,7 +7,7 @@ const validadeRideFields = (req, res, next) => {
   const {body} = req;
   
   const rideError = checkRequiredFields(body, rideRequiredFields);
-  if (rideError) return res.status(400).json({ message: `Os dados fornecidos no corpo da requisição são inválidos: ${rideError}`})
+  if (rideError) return res.status(400).json({ error_code: "INVALID_DATA", error_description: `Os dados fornecidos no corpo da requisição são inválidos: ${rideError}`})
 
   next();
 };
