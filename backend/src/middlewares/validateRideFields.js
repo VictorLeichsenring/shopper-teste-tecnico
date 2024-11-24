5030b839dbc72a653b5e73cfb1a2c5da9831723f
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-11-24 11:35:02 -0300

    Incluido erro para verificar se o endereço preenchido não está com espaços em branco

--- backend/src/middlewares/validateRideFields.js
+++ backend/src/middlewares/validateRideFields.js
@@ -10,6 +10,8 @@ const validadeRideFields = (req, res, next) => {
   if (rideError) return res.status(400).json({ error_code: "INVALID_DATA", error_description: `Os dados fornecidos no corpo da requisição são inválidos: ${rideError}`})
   
   if(body.origin === body.destination) return res.status(400).json({ error_code: "INVALID_DATA", error_description: 'Os dados fornecidos no corpo da requisição são inválidos: endereços de origem e destino iguais'})
+
+  if(body.origin.trim() ==='' || body.destination.trim() ==='') return res.status(400).json({ error_code: "INVALID_DATA", error_description: 'Os dados fornecidos no corpo da requisição são inválidos: um dos endereços está em branco'})
   next();
 };
 
